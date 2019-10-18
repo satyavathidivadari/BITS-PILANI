5f76c7b211efce6a856f52f2092b5c017ce929ee
Satya <56667493+satyavathidivadari@users.noreply.github.com>
2019-10-18 09:29:08 +0530

    Update Copy-index.php
    TESTING CHANGES to BRANCH with new comment

--- simple-php-website-master/newtemplate/Copy-index.php
+++ simple-php-website-master/newtemplate/Copy-index.php
@@ -1,6 +1,7 @@
 <?php
 // Comment these lines to hide errors
 // Addtional lines added to next changes - Satyavathi Divadari
+// TESTING CHANGES to BRANCH with new comment
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 require 'includes/config.php';
