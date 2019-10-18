d1ab2761b9ad82b91635eba348382f84154af067
Satya <56667493+satyavathidivadari@users.noreply.github.com>
2019-10-18 09:17:12 +0530

    Create Copy-index.php
    Copy of index file to test move

---
+++ simple-php-website-master/Copy-index.php
@@ -0,0 +1,8 @@
+<?php
+// Comment these lines to hide errors
+// Addtional lines added to next changes - Satyavathi Divadari
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+require 'includes/config.php';
+require 'includes/functions.php';
+init();
